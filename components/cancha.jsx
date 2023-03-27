675960845555cb1dedc6289a0627eb862d358220
Frank Cairampoma <ingfralch@gmail.com>
2023-03-27 17:16:25 -0500

    interfaz de inicio

---
+++ components/cancha.jsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+
+function Cancha() {
+  return (
+    <View style={styles.container}>
+      <View style={{flexDirection:"row", paddingVertical:10}}>
+        <Image  source={{ uri: 'https://portal.andina.pe/EDPfotografia/Thumbnail/2013/03/15/000205479W.jpg' }} 
+                style={{ width: 130, height: 100 , marginLeft:10, 
+                        borderRadius:10, borderWidth:3, borderColor:"white",
+                        shadowColor: '#000',
+                        shadowOffset: {
+                        width: 0,
+                        height: 2,
+                        },
+                        shadowOpacity: 0.25,
+                        shadowRadius: 3.84,
+
+                        elevation: 5,
+                    }} 
+        />
+        <View style={{flexDirection:"column", padding:10}}>
+            <Text style={{ backgroundColor:"#94C11C", fontSize:20, width:180, color:"white",
+              borderWidth: 1,
+              borderColor: '#94C11C',              
+              paddingLeft:10,
+          }}>Cancha 1</Text>
+            <Text style={{}}>Calle 1</Text>
+            <Text style={{}}>8:00am - 10:00pm</Text>
+
+        </View>
+
+
+
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        marginVertical: 8,
+        borderRadius: 10,
+        marginHorizontal: 10,
+
+      
+    },
+    text: {
+        color: 'black',
+        fontSize: 20,
+    },
+});
+
+export default Cancha;
--- components/home.jsx
+++ components/home.jsx
@@ -1,12 +1,67 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+
+import Cancha from './cancha';
+
+import { Ionicons } from '@expo/vector-icons';
 
 function Home() {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const handleSearch = (text) => {
+    setSearchQuery(text);
+    // Aquí puedes agregar la lógica para realizar la búsqueda
+  };
+
   return (
-    <View>
-      <Text>Hola</Text>
+    <View
+      style={[
+        styles.container,
+        {
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={{ flex: 1, flexDirection: 'row', alignItems: "center" }} >
+        <Image source={{ uri: 'https://futbolcracks.com/wp-content/uploads/2020/04/logo_FUTBOLCRACKS.COM_4_20_AF_Mesa-de-trabajo-1.png' }}
+          style={{ width: 80, height: 100 }} />
+        <TextInput
+          style={{ height: 40, 
+                  borderWidth: 2, 
+                  borderColor: '#94C11C', 
+                  width: "65%", 
+                  backgroundColor: 'white', 
+                  borderRadius: 10 }}
+          placeholder="Buscar..."
+          onChangeText={handleSearch}
+          value={searchQuery}
+        />
+        <TouchableOpacity >
+          <Ionicons name="search-circle" size={54} color="#94C11C" />
+        </TouchableOpacity>
+
+      </View>
+      <View style={{ flex: 5}} >
+        <ScrollView  showsVerticalScrollIndicator={false} >
+          <Cancha />
+          <Cancha />
+          <Cancha />
+          <Cancha />
+          <Cancha />
+          <Cancha />
+          <Cancha />
+          <Cancha />
+          <Cancha />
+          <Cancha />
+        </ScrollView>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
 
 export default Home;
