beafa9e116e0d03e8708220e8a0b6d7322e85075
Frank Cairampoma <ingfralch@gmail.com>
2023-03-29 11:58:04 -0500

    picker date done

--- components/calendario.jsx
+++ components/calendario.jsx
@@ -1,20 +1,24 @@
 // Description: Componente que muestra el dia actual y las horas disponibles para reservar
 import * as React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image, TextInput, Picker } from 'react-native';
-import { Picker as RNPicker } from '@react-native-picker/picker';
+import { View, Text, StyleSheet, TouchableOpacity, 
+        ScrollView, Image, TextInput, Picker, Button
+    } from 'react-native';
+
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 
 function Calendario() {
     const [dia, setDia] = React.useState('Lunes');
     const [horas, setHoras] = React.useState(['8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '24:00']);
 
-    const [selectedValue, setSelectedValue] = React.useState("");
+    const [date, setDate] = React.useState(new Date());
+    const [showDatePicker, setShowDatePicker] = React.useState(false);
 
-    const opciones = [
-        { label: 'Opción 1', value: 'opcion1' },
-        { label: 'Opción 2', value: 'opcion2' },
-        { label: 'Opción 3', value: 'opcion3' }
-    ];
+    const handleDateChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShowDatePicker(false);
+        setDate(currentDate);
+    };
 
     return (
         <View
@@ -25,19 +29,22 @@ function Calendario() {
                 },
             ]}>
             <View style={{ flex: 1, flexDirection: 'row', alignItems: "center" }} >
-               
+           
                 <Image source={require('../assets/logo.png')} style={{ width: 80, height: 100 }} />
             </View>
             <View style={{ flex: 5 }} >
                 <ScrollView showsVerticalScrollIndicator={false} >
-                <RNPicker
-                    selectedValue={selectedValue}
-                    onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-                >
-                    {opciones.map((opcion, index) => (
-                        <RNPicker.Item key={index} label={opcion.label} value={opcion.value} />
-                    ))}
-                </RNPicker>
+                <Button title="Select a date" onPress={() => setShowDatePicker(true)} />
+      {showDatePicker && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode="date"
+          is24Hour={true}
+          display="default"
+          onChange={handleDateChange}
+        />
+      )}
 
                 </ScrollView>
             </View>
