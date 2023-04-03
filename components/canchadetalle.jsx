fb33909b3791aeaf54978b6194ba5bdb0e6eed85
Frank Cairampoma <ingfralch@gmail.com>
2023-04-03 17:42:09 -0500

    boton flotante

--- components/canchadetalle.jsx
+++ components/canchadetalle.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 
 function CanchaDetalle(props) {
 
@@ -17,17 +17,99 @@ function CanchaDetalle(props) {
                 </Text>
             </View>
             <View style={{ flex: 5 }} >
+            <ScrollView  showsVerticalScrollIndicator={false}>
+            <View style={{
+                    flexDirection: 'column',
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    backgroundColor: '#fff',
+                    borderRadius: 10,
+                    margin: 10,
+                    padding: 10,
+                    elevation: 5,
+                }}>
+                    <Image source={{uri:"http://mail.mpi.gob.pe/images/Nota_706_VILLA_EL_EDEN_2.JPG"}} style={{ width: '95%', height: 200, marginLeft: 10 }} />
+                    <Text style={{fontSize: 18, fontWeight: 'bold', marginRight: 20,}}>Sed ut perspiciatis unde</Text>
+                    <Text style={{fontSize: 18,  marginRight: 20,}}>velit, sed quia non numquam</Text>
+
+                </View>
+                <View style={{
+                    flexDirection: 'column',
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    backgroundColor: '#fff',
+                    borderRadius: 10,
+                    margin: 10,
+                    padding: 10,
+                    elevation: 5,
+                }}>
+                    <Image source={{uri:"http://mail.mpi.gob.pe/images/Nota_706_VILLA_EL_EDEN_2.JPG"}} style={{ width: '95%', height: 200, marginLeft: 10 }} />
+                    <Text style={{fontSize: 18, fontWeight: 'bold', marginRight: 20,}}>Sed ut perspiciatis unde</Text>
+                    <Text style={{fontSize: 18,  marginRight: 20,}}>velit, sed quia non numquam</Text>
+
+                </View>
+                <View style={{
+                    flexDirection: 'column',
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    backgroundColor: '#fff',
+                    borderRadius: 10,
+                    margin: 10,
+                    padding: 10,
+                    elevation: 5,
+                }}>
+                    <Image source={{uri:"http://mail.mpi.gob.pe/images/Nota_706_VILLA_EL_EDEN_2.JPG"}} style={{ width: '95%', height: 200, marginLeft: 10 }} />
+                    <Text style={{fontSize: 18, fontWeight: 'bold', marginRight: 20,}}>Sed ut perspiciatis unde</Text>
+                    <Text style={{fontSize: 18,  marginRight: 20,}}>velit, sed quia non numquam</Text>
+
+                </View>
+                <View style={{
+                    flexDirection: 'column',
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    backgroundColor: '#fff',
+                    borderRadius: 10,
+                    margin: 10,
+                    padding: 10,
+                    elevation: 5,
+                }}>
+                    <Image source={{uri:"http://mail.mpi.gob.pe/images/Nota_706_VILLA_EL_EDEN_2.JPG"}} style={{ width: '95%', height: 200, marginLeft: 10 }} />
+                    <Text style={{fontSize: 18, fontWeight: 'bold', marginRight: 20,}}>Sed ut perspiciatis unde</Text>
+                    <Text style={{fontSize: 18,  marginRight: 20,}}>velit, sed quia non numquam</Text>
+
+                </View>
+            </ScrollView>
+                <TouchableOpacity style={styles.floatingButton}>
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
 
 }
 const styles = StyleSheet.create({
-    container: {
+    container:   {
         flex: 1,
         paddingHorizontal: 10,
         paddingVertical: 20,
     },
+    floatingButton: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 20,
+    bottom: 20,
+    backgroundColor: 'blue',
+    borderRadius: 30,
+    elevation: 3, // Agrega sombra al botón
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
 });
 
 export default CanchaDetalle;
