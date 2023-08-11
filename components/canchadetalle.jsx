7e65afba1e75c47589dfb03d439934df2c16e04e
Frank Cairampoma <ingfralch@gmail.com>
2023-08-11 17:36:44 -0500

    editando detalle

--- components/canchadetalle.jsx
+++ components/canchadetalle.jsx
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 
 function CanchaDetalle(props) {
-    const [data_fotos, setData_fotos] = useState(JSON.parse(props.route.params.fotos));
+    const [data_fotos, setData_fotos] = useState([
+            { "descripcion": "esta es la descripcion", "fotos": "https://d2bqn2kyidxvh4.cloudfront.net/wp-content/uploads/2021/02/antapaccay-808x454.jpg" }, 
+            { "descripcion": "Nueva descripción ", "fotos": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp01-QdJ8jHug8BhuLRp07L_Jwqfqz9G1o3C4XeJhszTO5votBKg1XkMbTtGbtSOOg0Go&usqp=CAU" }, 
+            { "descripcion": "Descripción de las canchitas ", "fotos": "https://lostanos.com/wp-content/uploads/2019/06/Vista-Hilo.jpg" }, 
+            { "descripcion": "Canchita deportiva de fútbol ", "fotos": "https://www.abc.com.py/resizer/-av8fIxAay5xcUlBoJpxMtK0vqo=/fit-in/770x495/smart/filters:format(webp)/arc-anglerfish-arc2-prod-abccolor.s3.amazonaws.com/public/RNJISPSCDZGDVOF2XPL44M2ETY.jpg" }
+        ]);
+
+
 
     return (
         <View style={styles.container}>
@@ -33,7 +40,7 @@ function CanchaDetalle(props) {
                                     padding: 10,
                                     elevation: 5,
                                 }}>
-                                    <Image source={{ uri: "http://162.248.55.24:3000/uploads/" + foto.fotos }} style={{ width: '95%', height: 200, marginLeft: 10 }} />
+                                    <Image source={{ uri: "" + foto.fotos }} style={{ width: '95%', height: 200, marginLeft: 10 }} />
                                     <Text style={{ fontSize: 18, marginRight: 20, }}>{foto.descripcion}</Text>
                                 </View>
                             ))
